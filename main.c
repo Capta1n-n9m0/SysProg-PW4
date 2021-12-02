d1fe96d267d53baecf3b33ff0cf025303ce5f4c9
Capta1n-n9m0 <alievabbas1@gmail.com>
2021-12-02 10:40:59 +0400

    Finished pw4(file test)

--- main.c
+++ main.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include "my_file.h"
 
 int main(){
     MY_FILE *f1 = my_fopen("foo1", "r");
+    if(f1 == NULL){
+        perror("foo1");
+        exit(1);
+    }
     MY_FILE *f2 = my_fopen("foo2", "r");
-    int c1, c2;
-    while((c2 = my_getc(f2)) != EOF){
+    if(f2 == NULL){
+        perror("foo2");
+        exit(1);
+    }
+    int c1, c2, len;
+    len = f1->size;
+    puts("\nfoo1");
+    for(int i = 0; i < len; i++){
         c1 = my_getc(f1);
-        printf("%c %c  ", c1, c2);
+        printf("%c", c1);
+    }
+    puts("\nfoo2");
+    for(int i = 0; i < len; i++){
+        c2 = my_getc(f2);
+        printf("%c", c2);
     }
     puts("");
     my_fclose(f1);
